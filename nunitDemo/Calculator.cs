c75c88d36bb4784f2e0f15de15390746b454732f
Shubham Kumar <shubham.kumar89@outlook.com>
2021-06-14 15:07:45 +0530

    Add project files.

---
+++ nunitDemo/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace nunitDemo
+{
+    public class Calculator : IDisposable
+    {
+        public int add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public void Dispose()
+        {
+            
+        }
+
+        public int subtract(int n1, int n2)
+        {
+            if(n1<n2)
+            {
+                throw new ArgumentException("First number should be greater than second number");
+            }
+            else
+            {
+                return n1 - n2;
+            }
+        }
+    }
+}
---
+++ nunitProj/CalculatorTest.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using NUnit.Framework;
+using nunitDemo;
+
+namespace nunitProj
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        Calculator calc;
+        [SetUp]
+        public void SetUp()
+        {
+            calc = new Calculator();
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            calc.Dispose();
+        }
+
+
+        [Test, Order(3)]
+        public void Test_Add_Method()
+        {
+            var result = calc.add(20, 40);
+            Assert.AreEqual(60, result);
+        }
+
+
+        [Test, Order(1)]
+        public void Test_Sustract_Method()
+        {
+            //var sub_result = calc.subtract(20, 40);
+            //Assert.AreEqual(20, sub_result);
+            Assert.Catch<SystemException>(() => calc.subtract(3, 4));
+            Assert.Throws<ArgumentException>(() => calc.subtract(3, 4));
+        }
+
+
+        [TestCase(2,3,5),Order(2)]
+        [TestCase(5, 5, 10)]
+        [TestCase(6, 6, 12)]
+        public void Multiple_argument_passed(int n1, int n2, int expectedResult)
+        {
+            var Add_Result = calc.add(n1, n2);
+            Assert.AreEqual(expectedResult, Add_Result);
+        }
+
+
+        [Ignore("Ignore Test")]
+        public void Test_Method_To_Be_Ignored()
+        {
+
+        }
+    }
+}
